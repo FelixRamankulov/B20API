3db1c6daa6718c6b19083bca1eb8d6929f277883
FeliksRamankulov <letopis666@gmail.com>
2024-04-25 13:27:30 -0500

    added new files

--- src/test/java/get/FakeStore.java
+++ src/test/java/get/FakeStore.java
@@ -107,6 +107,73 @@ public class FakeStore {
 
     }
 
+    @Test
+    public void test4(){
+        String url = "https://covid-api.com/api/regions";
+        Response response = RestAssured.given().header("Accept","application/json")
+                .queryParam("per_page","20")
+                .queryParam("order","name")
+                .queryParam("sort","asc")
+                .when().get(url)
+                .then().statusCode(200)
+                .extract().response();
+
+        Map<String ,Object>  parsedResponse = response.as(new TypeRef<Map<String, Object>>() {
+        });
+
+        String  perPage = (String) parsedResponse.get("per_page");
+        Assert.assertEquals("20",perPage);
+
+
+        int total = (int) parsedResponse.get("total");
+        Assert.assertEquals(1002,total);
+
+        List<Map<String,String>> dataMap = (List<Map<String, String>>) parsedResponse.get("data");
+
+
+        for (int i = 0; i < dataMap.size(); i++) {
+            Map<String,String > tempMap = dataMap.get(i);
+            System.out.println(tempMap.get("name"));
+
+        }
+
+
+    }
+
+
+    @Test
+    public void test5(){
+
+        String url = "https://fakestoreapi.com/products";
+        Response response = RestAssured.given().header("Accept","application/json")
+                .when().get(url)
+                .then().statusCode(200)
+                .extract().response();
+
+        List<Map<String,Object>> productsList = response.as(new TypeRef<List<Map<String, Object>>>() {
+        });
+
+        for (int i = 0; i < productsList.size(); i++) {
+            Map<String,Object> temp = productsList.get(i);
+            String title = temp.get("title").toString();
+            double price = Double.parseDouble(temp.get("price").toString());
+            System.out.println("Title is: " + title);
+            System.out.println("Price is: " + price);
+
+            System.out.println(temp.get("description"));
+
+
+
+        }
+
+
+
+
+
+
+
+    }
+
 
 
 
